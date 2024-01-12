e423b4ca2a4c868ad1da75353797418ba05c9376
Clovis <clovisgarnier00@mail.com>
2024-01-12 15:29:13 +0100

    plein de trucs

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,11 +6,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact.index')]
-    public function index(): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
+        $name = $request->request->get('name');
+        $Theemail = $request->request->get('email');
+        $message = $request->request->get('message');
+        
+        $email = (new Email())
+            ->from('binjoans65@gmail.com')
+            ->to('joanlekeke@gmail.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $mailer->send($email);
+        
+
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
         ]);
