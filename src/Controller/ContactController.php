648f9aa712da00d54bdd48e6b63c7991c542acc6
Clovis <clovisgarnier00@mail.com>
2024-01-15 00:10:59 +0100

    de la merde, fais gaffe j'ai peutetre pas switch la bdd

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -9,29 +9,14 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Address;
 
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact.index')]
-    public function index(Request $request, MailerInterface $mailer): Response
+    public function index(): Response
     {
-        $name = $request->request->get('name');
-        $Theemail = $request->request->get('email');
-        $message = $request->request->get('message');
-        
-        $email = (new Email())
-            ->from('binjoans65@gmail.com')
-            ->to('joanlekeke@gmail.com')
-            //->cc('cc@example.com')
-            //->bcc('bcc@example.com')
-            //->replyTo('fabien@example.com')
-            //->priority(Email::PRIORITY_HIGH)
-            ->subject('Time for Symfony Mailer!')
-            ->text('Sending emails is fun again!')
-            ->html('<p>See Twig integration for better HTML integration!</p>');
-
-        $mailer->send($email);
         
 
         return $this->render('contact/index.html.twig', [
